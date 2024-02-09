83011b94971ef8114896c0509c19663cb8de1473
Priscila Schleske <plasch7@gmail.com>
2024-02-08 21:34:12 -0700

    x

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline"> HELLO !</h1>
+      <h1 className="text-3xl font-bold underline"> Saguaro Sweets</h1>
     </>
   )
 }
